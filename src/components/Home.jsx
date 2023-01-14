735f11112333c5ae343a556de87276dee8583237
Raghav <raghavtechlover@gmail.com>
2023-01-14 08:28:38 +0530

    Added Avatar and Footer

---
+++ src/components/Home.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import HeroImage from "../assets/heroImage.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
+import { useEffect, useState } from "react";
+import Typewriter from "typewriter-effect/dist/core";
+
+const titles = ["Flutter Developer", "Web Enthusiast", "Python Programmer"];
+
+const Home = () => {
+  const [titleIndex, setTitleIndex] = useState(0);
+  const [currentTitle, setCurrentTitle] = useState(titles[0]);
+  let typewriter;
+  useEffect(() => {
+    typewriter = new Typewriter(document.getElementById("typewriter"), {
+      loop: true,
+      delay: 300,
+    });
+    titles.forEach((title) => {
+      typewriter.typeString(title).pauseFor(1000).deleteAll();
+    });
+    typewriter.start();
+  }, []);
+
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          {/* <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            Hi! Iam Raghav.\nI am a{" "}
+            <span id="typewriter">{titles[titleIndex]}</span>
+          </h2> */}
+          <div>
+            <p className="text-4xl sm:text-7xl font-bold text-white">
+              {" "}
+              Hi! I'm Raghav.
+            </p>
+            <p className="text-4xl sm:text-7xl font-bold text-white">
+              {" "}
+              I am a <span id="typewriter"></span>
+            </p>
+          </div>
+          <p className="text-gray-500 py-4 max-w-md">Lorem Ipsum.....</p>
+          <div>
+            <Link
+              to="projects"
+              duration={700}
+              smooth
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer "
+            >
+              Projects
+              <span className="group-hover:rotate-90 duration-500">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </Link>
+          </div>
+        </div>
+        <div>
+          {/* <img
+            src={HeroImage}
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
+          /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
