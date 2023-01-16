3915cbe1e522dd5497a1f55d88fc5a30ee336c84
Raghav <raghavtechlover@gmail.com>
2023-01-16 15:14:30 +0530

    Added Resume in mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,8 +16,8 @@ const NavBar = () => {
     {
       id: 2,
       title: "about",
-      offset: -90,
-      offsetMobile: -205,
+      offset: -110,
+      offsetMobile: -240,
     },
     {
       id: 3,
@@ -28,13 +28,19 @@ const NavBar = () => {
     {
       id: 4,
       title: "skills",
-      offset: -55,
+      offset: -140,
       offsetMobile: 85,
     },
     {
       id: 5,
+      title: "resume",
+      offset: 0,
+      offsetMobile: 0,
+    },
+    {
+      id: 6,
       title: "contact",
-      offset: 90,
+      offset: 70,
       offsetMobile: 330,
     },
   ];
@@ -44,16 +50,19 @@ const NavBar = () => {
         <h1 className="text-5xl font-signature ml-2">Raghav</h1>
       </div>
       <ul className="hidden md:flex">
-        {links.map((link) => (
-          <li
-            key={link.id}
-            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
-          >
-            <Link to={link.title} smooth offset={link.offset} duration={700}>
-              {link.title}
-            </Link>
-          </li>
-        ))}
+        {links
+          .slice(0, 4)
+          .concat(links.slice(5))
+          .map((link) => (
+            <li
+              key={link.id}
+              className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+            >
+              <Link to={link.title} smooth offset={link.offset} duration={700}>
+                {link.title}
+              </Link>
+            </li>
+          ))}
       </ul>
       <div
         onClick={() => setNav(!nav)}
@@ -64,7 +73,29 @@ const NavBar = () => {
 
       {nav && (
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
-          {links.map((link) => (
+          {links.slice(0, 5).map((link) => (
+            <li
+              key={link.id}
+              className="px-4 capitalize cursor-pointer py-6 text-4xl"
+            >
+              {link.id === 5 ? (
+                <a href={"http://bit.ly/3ZDIMvu"} target="_blank">
+                  {link.title}
+                </a>
+              ) : (
+                <Link
+                  onClick={() => setNav(!nav)}
+                  to={link.title}
+                  smooth
+                  offset={link.offsetMobile}
+                  duration={700}
+                >
+                  {link.title}
+                </Link>
+              )}
+            </li>
+          ))}
+          {links.slice(5).map((link) => (
             <li
               key={link.id}
               className="px-4 capitalize cursor-pointer py-6 text-4xl"
